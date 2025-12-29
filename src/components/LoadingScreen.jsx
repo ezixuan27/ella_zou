0c0a537aaf7e073ed66b92f1cffd57dd0adee810
ezixuan27 <ezixuan27@gmail.com>
2025-12-29 14:36:12 -0600

    blinking effect update

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,42 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
+import Lottie from "lottie-react";
+import eyeAnimation from "../assets/eye-animation.json"; 
 
 export const LoadingScreen = ({ onComplete }) => {
-    const [text, setText] = useState("");
-    const fullText = "<Hello World />";
+    const lottieRef = useRef();
 
     useEffect(() => {
-    document.body.classList.add("overflow-hidden");
-    return () => document.body.classList.remove("overflow-hidden");
-  }, []);
+        document.body.classList.add("overflow-hidden");
+        if (lottieRef.current) {
+            lottieRef.current.setSpeed(0.8); 
+        }
 
-    useEffect(() => {
-        let index = 0;
-        const interval = setInterval(() => {
-            setText(fullText.substring(0, index))
-            index++;
-
-            if (index > fullText.length) {
-                clearInterval(interval);
-
-                setTimeout(() => {
-                    onComplete();
-                }, 1000);
-            }
-        }, 100);
-
-        return () => clearInterval(interval);
-    }, [onComplete])
-
-
+        return () => document.body.classList.remove("overflow-hidden");
+    }, []);
 
     return (
-        <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center"> 
-            <div className="mb-4 text-4xl font-mono font-bold">
-                {text} <span className="animate-blink ml-1"> | </span>
-            </div>
-
-            <div className="w-[350px] h-[2px] bg-gray-800 rounded relative overflow-hiddent">
-                <div className="w-[20%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"> </div>
+        <div className="fixed inset-0 z-50 bg-black flex items-center justify-center">
+            <div className="h-screen"> 
+                <Lottie 
+                    lottieRef={lottieRef}
+                    animationData={eyeAnimation}
+                    loop={false} 
+                    autoplay={true}
+                    onComplete={() => {
+                        onComplete();
+                    }}
+                />
             </div>
         </div>
     );
